313a287dcf8e9367f142d7a8bb022492a498b36a
Washington Botelho <wbotelhos@gmail.com>
2021-07-29 21:18:01 -0300

    Installs Absinthe

--- mix.exs
+++ mix.exs
@@ -42,7 +42,9 @@ defmodule GraphqlWithAbsintheOnPhoenix.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:absinthe, "~> 1.6"},
+      {:absinthe_plug, "~> 1.5"}
     ]
   end
 
