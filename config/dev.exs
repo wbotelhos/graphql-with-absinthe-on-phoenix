a9de039222cec2529ab33f02fe900516bc8d824a
Washington Botelho <wbotelhos@gmail.com>
2021-07-29 21:18:59 -0300

    Adds PG docker compose

--- config/dev.exs
+++ config/dev.exs
@@ -3,7 +3,7 @@ use Mix.Config
 # Configure your database
 config :graphql_with_absinthe_on_phoenix, GraphqlWithAbsintheOnPhoenix.Repo,
   username: "postgres",
-  password: "postgres",
+  password: "",
   database: "graphql_with_absinthe_on_phoenix_dev",
   hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
