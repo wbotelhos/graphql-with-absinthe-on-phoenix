069a24422a78ffb4db8b8fdae67b07a0ecda8a47
Washington Botelho <wbotelhos@gmail.com>
2021-07-31 01:11:00 -0300

    Sort

--- mix.exs
+++ mix.exs
@@ -33,19 +33,19 @@ defmodule GraphqlWithAbsintheOnPhoenix.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.5.9"},
-      {:phoenix_ecto, "~> 4.1"},
+      {:absinthe_plug, "~> 1.5"},
+      {:absinthe, "~> 1.6"},
       {:dataloader, "~> 1.0"},
       {:ecto_sql, "~> 3.4"},
-      {:postgrex, ">= 0.0.0"},
-      {:phoenix_live_dashboard, "~> 0.4"},
-      {:telemetry_metrics, "~> 0.4"},
-      {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
+      {:phoenix_ecto, "~> 4.1"},
+      {:phoenix_live_dashboard, "~> 0.4"},
+      {:phoenix, "~> 1.5"},
       {:plug_cowboy, "~> 2.0"},
-      {:absinthe, "~> 1.6"},
-      {:absinthe_plug, "~> 1.5"}
+      {:postgrex, ">= 0.0.0"},
+      {:telemetry_metrics, "~> 0.4"},
+      {:telemetry_poller, "~> 0.4"}
     ]
   end
 
