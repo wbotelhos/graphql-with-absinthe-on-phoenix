5ab4d44b55e0cb8e7763dad138f3fcbaa9cf1039
Washington Botelho <wbotelhos@gmail.com>
2021-08-01 21:09:47 -0300

    Creates user

---
+++ priv/repo/migrations/20210801230139_create_users.exs
@@ -0,0 +1,13 @@
+defmodule GraphqlWithAbsintheOnPhoenix.Repo.Migrations.CreateUsers do
+  use Ecto.Migration
+
+  def change do
+    create table(:users) do
+      add :email, :string
+      add :password, :string
+
+      timestamps()
+    end
+
+  end
+end
