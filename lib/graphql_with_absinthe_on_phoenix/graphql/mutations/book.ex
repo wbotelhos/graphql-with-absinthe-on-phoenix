f831d4c78bbba821ef2dd6deee5e3640aff864a5
Washington Botelho <wbotelhos@gmail.com>
2021-08-08 11:44:47 -0300

    Middleware to authenticate the user

--- lib/graphql_with_absinthe_on_phoenix/graphql/mutations/book.ex
+++ lib/graphql_with_absinthe_on_phoenix/graphql/mutations/book.ex
@@ -2,6 +2,7 @@ defmodule GraphqlWithAbsintheOnPhoenix.GraphQL.Mutations.Book do
   use Absinthe.Schema.Notation
 
   alias GraphqlWithAbsintheOnPhoenix.GraphQL.Resolvers
+  alias GraphqlWithAbsintheOnPhoenix.Middlewares
 
   object :book_mutations do
     field :create_book, :book do
@@ -9,6 +10,8 @@ defmodule GraphqlWithAbsintheOnPhoenix.GraphQL.Mutations.Book do
       arg(:position, :integer)
 
       arg(:verses, list_of(:verse_create_inputs))
+
+      middleware(Middlewares.Authenticator)
 
       resolve(&Resolvers.Book.create_book/2)
     end
